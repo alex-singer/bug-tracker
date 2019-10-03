10b277e0cce2b4d6b6fca845a370dc562ca19da6
Alex Singer <alex@asinger.me>
2019-10-03 08:24:32 -0500

    Working API withOUT database integration

--- server/server.js
+++ server/server.js
@@ -4,9 +4,31 @@ const fs = require("fs");
 
 let aboutMessage = "Issue Tracker API v1.0";
 
+const issuesDB = [
+  {
+    id: 1,
+    status: "New",
+    owner: "Raven",
+    effort: 5,
+    created: new Date('2019-10-01'),
+    due: undefined,
+    title: "What the heck is going on?",
+  },
+  {
+    id: 2,
+    status: "New",
+    owner: "Alex",
+    effort: 5,
+    created: new Date('2019-10-01'),
+    due: new Date('2019-10-10'),
+    title: "What the heckin heck is going on?",
+  },
+];
+
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setAboutMessage,
@@ -15,6 +37,10 @@ const resolvers = {
 
 function setAboutMessage(_, { message }) {
   return aboutMessage = message;
+}
+
+function issueList() {
+  return issuesDB;
 }
 
 const server = new ApolloServer({
