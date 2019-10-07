137c073892eb287ac00e456730c7cf8dd6afd1a2
Alex Singer <alex@asinger.me>
2019-10-07 10:08:28 -0500

    Implement HMR

--- ui/public/App.js
+++ ui/public/App.js
@@ -12,6 +12,10 @@ var _IssueList = _interopRequireDefault(require("./IssueList.jsx"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 var element = _react["default"].createElement(_IssueList["default"], null);
 
 _reactDom["default"].render(element, document.getElementById('contents'));
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -5,6 +5,10 @@ import ReactDOM from "react-dom";
 
 import IssueList from './IssueList.jsx';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const element = <IssueList />;
 
 ReactDOM.render(element, document.getElementById('contents'));
