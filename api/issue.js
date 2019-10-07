d1a167fa61280be91c4748e7c3db161e274dd4c5
Alex Singer <alex@asinger.me>
2019-10-07 08:20:48 -0500

    API directory broken into proper seperate files

---
+++ api/issue.js
@@ -0,0 +1,35 @@
+const { getDb, getNextSequence } = require("./db");
+const { UserInputError } = require('apollo-server-express');
+
+async function add(_, { issue }) {
+  const db = getDb();
+  validate(issue);
+  const newIssue = { ...issue };
+  newIssue.created = new Date();
+  newIssue.id = await getNextSequence('issues');
+  const result = await db.collection('issues').insertOne(newIssue);
+  const savedIssue = await db.collection('issues')
+    .findOne({ _id: result.insertedId });
+  return savedIssue;
+}
+
+function validate(issue) {
+  const errors = [];
+  if (issue.title.length < 3) {
+    errors.push("Field 'title' must be at least 3 characters long.");
+  }
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push("Field 'owner' is required when status is 'Assigned'");
+  }
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors });
+  }
+}
+
+async function list() {
+  const db = getDb();
+  const issues = await db.collection('issues').find({}).toArray();
+  return issues;
+}
+
+module.exports = { list, add };
