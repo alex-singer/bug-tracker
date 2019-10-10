ce0ab324d274d2e0c830b931394aef539b017aee
Alex Singer <alex@asinger.me>
2019-10-10 10:28:43 -0500

    Implement Browser History Routing

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const proxy = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
 
@@ -36,6 +37,10 @@ const env = { UI_API_ENDPOINT };
 
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
 });
 
 const port = process.env.UI_SERVER_PORT || 8000;
